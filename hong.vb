e661bc367a3889b6df8556b1c196dbbf0c23c29a
invisifire <59678894+invisifire@users.noreply.github.com>
2022-01-08 14:59:56 +0800

    Add files via upload
    2

--- hong.vb
+++ hong.vb
@@ -1,27 +1,3 @@
-Sub Macro1()
-'
-    Sheets("已确认阳性库").Columns("A:AM").Delete Shift:=xlShiftToLeft
-    Sheets("初筛阳性库").Columns("A:AM").Copy Sheets("已确认阳性库").[A1]
-    Sheets("已确认阳性库").Range("A1").AutoFilter
-    With Sheets("已确认阳性库").AutoFilter.Sort
-        With .SortFields
-            .Clear
-            .Add Key:=Range("V1"), Order:=xlDescending' Range("V1") 初筛阳性日期
-        End With
-        .Header = xlYes
-        .MatchCase = False
-        .SortMethod = xlPinYin
-        .Orientation = xlSortColumns
-        .Apply
-    End With
-    Application.CutCopyMode = False
-    Sheets("已确认阳性库").Columns("A:AM").RemoveDuplicates Columns:=2, Header:=xlYes
-    'Columns:=2是姓名列号
-    Sheets("已确认阳性库").Columns("A:AM").AutoFilter Field:=29, Criteria1:=Array("确诊病例", "无症状感染者"), Operator:=xlFilterValues
-    'Field:=29是目前最终分类
-    ActiveWorkbook.Names.Add Name:="已确认阳性库!_FilterDatabase", RefersTo:="=已确认阳性库!$A:$AM", Visible:=False
-    'Range("A1").Select
-End Sub
 
 Sub mijie()
 '
@@ -54,4 +30,29 @@ Sub mijie()
     'Field:=18是病例复核结果 此句为选择18列筛选阳性
     ActiveWorkbook.Names.Add Name:="'确认密接库-先不录'!_FilterDatabase", RefersTo:="='确认密接库-先不录'!$A:$Z", Visible:=False
     'Range("A1").Select 此句为将筛选结果保存更新到表带
+End Sub
+
+Sub Macro1()
+'
+    Sheets("已确认阳性库").Columns("A:AM").Delete Shift:=xlShiftToLeft
+    Sheets("初筛阳性库").Columns("A:AM").Copy Sheets("已确认阳性库").[A1]
+    Sheets("已确认阳性库").Range("A1").AutoFilter
+    With Sheets("已确认阳性库").AutoFilter.Sort
+        With .SortFields
+            .Clear
+            .Add Key:=Range("V1"), Order:=xlDescending' Range("V1") 初筛阳性日期
+        End With
+        .Header = xlYes
+        .MatchCase = False
+        .SortMethod = xlPinYin
+        .Orientation = xlSortColumns
+        .Apply
+    End With
+    Application.CutCopyMode = False
+    Sheets("已确认阳性库").Columns("A:AM").RemoveDuplicates Columns:=2, Header:=xlYes
+    'Columns:=2是姓名列号
+    Sheets("已确认阳性库").Columns("A:AM").AutoFilter Field:=29, Criteria1:=Array("确诊病例", "无症状感染者"), Operator:=xlFilterValues
+    'Field:=29是目前最终分类
+    ActiveWorkbook.Names.Add Name:="已确认阳性库!_FilterDatabase", RefersTo:="=已确认阳性库!$A:$AM", Visible:=False
+    'Range("A1").Select
 End Sub
